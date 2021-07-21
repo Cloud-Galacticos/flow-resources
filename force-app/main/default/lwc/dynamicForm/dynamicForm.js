832944bd62d79a6d58e4aba19e8aff372aa4b87e
Michael Gill <mike@cloudgalacticos.co.uk>
2021-07-21 12:52:02 +0100

    DynamicForm using CMT

--- force-app/main/default/lwc/dynamicForm/dynamicForm.js
+++ force-app/main/default/lwc/dynamicForm/dynamicForm.js
@@ -2,30 +2,40 @@
  * @description       : dynamicForm
  * @author            : agentgill
  * @group             :
- * @last modified on  : 07-20-2021
+ * @last modified on  : 07-21-2021
  * @last modified by  : agentgill
  * Modifications Log
  * Ver   Date         Author      Modification
  * 1.0   07-14-2021   agentgill   Initial Version
+ * 
+TODO: Add custom button to call method
+TODO: New method generating JSON -  Now we need to create json based on the dymanic inputs
+    {
+      "My String Field": "Mike & Abdel",
+      "My Date field": "2021-07-21"
+}
+
+TODO: ON click of button - console log the JSON output
+  
  **/
 import { LightningElement } from 'lwc';
-
+import getFormFields from '@salesforce/apex/dynamicFormController.getForm';
 export default class DynamicForm extends LightningElement {
-    fields = [
-        {
-            FieldLabel: 'My String Field!',
-            FieldName: 'My String Field',
-            FieldType: 'Text'
-        },
-        {
-            FieldLabel: 'My Number Field!',
-            FieldName: 'My Number Field',
-            FieldType: 'Float'
-        },
-        {
-            FieldLabel: 'My Date Field!',
-            FieldName: 'My Date Field',
-            FieldType: 'Date'
+    fields;
+    error;
+    connectedCallback() {
+        this.loadFields();
+    }
+
+    async loadFields() {
+        // Modern async/await pattern for promise
+        console.log('running async/await function now');
+        try {
+            const myFields = await getFormFields();
+            console.log(myFields);
+            this.fields = JSON.parse(myFields);
+        } catch (error) {
+            console.log(error);
         }
-    ];
+    }
 }
