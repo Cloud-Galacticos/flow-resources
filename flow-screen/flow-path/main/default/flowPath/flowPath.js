0a2060d306dc1e4518a8019e17ff03e3941254b0
Michael Gill <mike@cloudgalacticos.co.uk>
2021-09-19 13:16:01 +0100

    reorg the repo

--- flow-screen/flow-path/main/default/flowPath/flowPath.js
+++ flow-screen/flow-path/main/default/flowPath/flowPath.js
@@ -2,17 +2,17 @@
  * @description       : FlowPath (https://developer.salesforce.com/docs/component-library/bundle/lightning-progress-indicator/documentation)
  * @author            : agentgill
  * @group             :
- * @last modified on  : 07-05-2021
+ * @last modified on  : 07-27-2021
  * @last modified by  : agentgill
  * Modifications Log
  * Ver   Date         Author      Modification
  * 1.0   07-05-2021   agentgill   Initial Version
  **/
-import { LightningElement, api } from "lwc";
+import { LightningElement, api } from 'lwc';
 
 export default class FlowPath extends LightningElement {
-  @api activeStages;
-  @api currentStage;
-  @api flowType;
-  @api flowVariant;
+    @api activeStages;
+    @api currentStage;
+    @api pathType;
+    @api pathVariant;
 }
